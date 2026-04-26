4c171b9eef0e6af27d1d7ce0fe643e1982ba7629
ThanmayaC <thanmayacharithachinnamgari@gmail.com>
2026-04-26 13:18:38 +0530

    Common db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/homigo");
-    console.log("MongoDB Connected ✅");
+    await mongoose.connect("mongodb+srv://thanmaya1256:Thanmaya1256@cluster0.fwe2vfi.mongodb.net/homigo");
+    console.log("MongoDB Connected (Atlas) ✅");
   } catch (err) {
     console.error(err);
     process.exit(1);
